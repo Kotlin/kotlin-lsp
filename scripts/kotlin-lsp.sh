35c11ba175c60ca46104a216c25c6ded917e5ffe
Bogdan Mukvich <bogdan.mukvich@jetbrains.com>
2025-05-19 18:06:37 +0200

    [lsp] run-script: add binary dir discovery
    GitOrigin-RevId: ca8a20b793d8bae7a161c79fb93b544af9792473

--- scripts/kotlin-lsp.sh
+++ scripts/kotlin-lsp.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-if [ ! -d "lib" ]; then
-  echo "The 'lib' directory does not exist."
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+  SOURCE=$(readlink "$SOURCE")
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE
+done
+DIR=$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
+
+if [ ! -d "$DIR/lib" ]; then
+  echo >&2 -e "The 'lib' directory does not exist."
   exit 1
 fi
 
-jars=$(find lib -name "*.jar" -type f | tr '\n' ':')
+jars=$(find "$DIR/lib" -name "*.jar" -type f | tr '\n' ':')
 
 # Remove the trailing colon from the classpath
 classpath=${jars%:}
