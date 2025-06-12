fe996b7c8005fd873d7dd42583d8635c9cd6e67c
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2025-06-12 14:43:14 +0200

    [lsp] implement integration tests for standalone kotlin lsp
    ^LSP-166 fixed
    
    GitOrigin-RevId: 44330d861a1a0f87eb0d24f4919184bedf1bdb83

--- kotlin-lsp/src/com/jetbrains/ls/kotlinLsp/KotlinLspServerRunConfig.kt
+++ kotlin-lsp/src/com/jetbrains/ls/kotlinLsp/KotlinLspServerRunConfig.kt
@@ -11,3 +11,13 @@ sealed interface KotlinLspServerMode {
 
     data class Socket(val config: TcpConnectionConfig) : KotlinLspServerMode
 }
+
+
+fun KotlinLspServerRunConfig.toArguments(): List<String> =
+    when (mode) {
+        is KotlinLspServerMode.Stdio -> listOf("--stdio")
+        is KotlinLspServerMode.Socket -> when (val tcpConfig = mode.config) {
+            is TcpConnectionConfig.Client -> listOf("--client", "--socket=${tcpConfig.port}")
+            is TcpConnectionConfig.Server -> listOf("--socket=${tcpConfig.port}")
+        }
+    }
