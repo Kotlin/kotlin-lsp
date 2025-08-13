e4dec96268221a0723c68c9d9620cc2ea5f3bf1f
pavel.mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2025-08-13 13:41:04 +0200

    [lsp] Make sure that the presense or absence of JDK sources doesn't affect tests
    GitOrigin-RevId: aa79cf7b5ef6c39900d50fcf06185819927d9150

--- api.core/src/com/jetbrains/ls/api/core/util/LSUtil.kt
+++ api.core/src/com/jetbrains/ls/api/core/util/LSUtil.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.ls.api.core.util
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.projectRoots.SdkType
 import com.intellij.openapi.roots.OrderRootType
@@ -107,14 +108,17 @@ fun addSdk(
             }
 
             val javaHome = roots.first().lspUriToIntellijUri()!!.substringAfter("://").substringBeforeLast("!/")
-            val srcZip = Path.of(FileUtilRt.toSystemDependentName(javaHome), "lib", "src.zip")
-            if (srcZip.exists()) {
-                val prefix = "jar://${FileUtilRt.toSystemIndependentName(srcZip.toString())}!/"
-                roots.mapTo(this) { root ->
-                    SdkRoot(
-                        urlManager.getOrCreateFromUrl("$prefix${root.uri.substringAfterLast("/")}"),
-                        SdkRootTypeId(OrderRootType.SOURCES.customName),
-                    )
+            // TODO: Apparently, we need to run all tests two times, with and without JDK sources
+            if (!ApplicationManager.getApplication().isUnitTestMode) {
+                val srcZip = Path.of(FileUtilRt.toSystemDependentName(javaHome), "lib", "src.zip")
+                if (srcZip.exists()) {
+                    val prefix = "jar://${FileUtilRt.toSystemIndependentName(srcZip.toString())}!/"
+                    roots.mapTo(this) { root ->
+                        SdkRoot(
+                            urlManager.getOrCreateFromUrl("$prefix${root.uri.substringAfterLast("/")}"),
+                            SdkRootTypeId(OrderRootType.SOURCES.customName),
+                        )
+                    }
                 }
             }
         },
