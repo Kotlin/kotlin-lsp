e320fe9de6d92618b94d6e9a472810fee67d5abb
Ludwig Valda Vasquez <ludwig.valda.vasquez@jetbrains.com>
2025-11-20 13:27:57 +0100

    refactor [lsp]: abstract over i/o to remove ktor dependency
    We want to remove ktor dependency from the lsp.protocol module as it
    conflicts with other uses.
    
    The module has a single usage of ktor, its i/o utils types:
    ByteReadChannel and ByteWriteChannel.
    
    These are now replaced by requivalent interfaces. In this patch, ktor
    dependency is still present and implements the aforementioned interfaces
    in this same module, but the goal is to move the implementation into
    module consumers.
    
    GitOrigin-RevId: 62681cd01c54b00cab7e17682fe6858db01d4186

--- kotlin-lsp/src/com/jetbrains/ls/kotlinLsp/stdio.kt
+++ kotlin-lsp/src/com/jetbrains/ls/kotlinLsp/stdio.kt
@@ -1,9 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.ls.kotlinLsp
 
-import com.jetbrains.lsp.implementation.LspConnection
-import io.ktor.utils.io.ByteWriteChannel
-import io.ktor.utils.io.cancel
+import com.jetbrains.lsp.implementation.*
 import io.ktor.utils.io.jvm.javaio.toByteReadChannel
 import io.ktor.utils.io.streams.asByteWriteChannel
 import kotlinx.coroutines.CoroutineScope
@@ -25,8 +23,8 @@ private class StdioConnection(
     inputStream: InputStream,
     outputStream: OutputStream,
 ) : LspConnection {
-    override val input = inputStream.toByteReadChannel()
-    override val output: ByteWriteChannel = outputStream.asByteWriteChannel()
+    override val input = KtorByteReader(inputStream.toByteReadChannel())
+    override val output: ByteWriter = KtorByteWriter(outputStream.asByteWriteChannel())
 
     override fun close() {
         input.cancel()
