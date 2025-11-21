66e6c9b4124d0709bc013bf960338f70fa765f7b
Tagir Valeev <tagir.valeev@jetbrains.com>
2025-11-21 17:20:19 +0100

    LSP-276 [lsp] (part of) Item kind for keywords and primitive types
    GitOrigin-RevId: 93bec419107d487f49aef129c42c9cc8a1c9bcd7

--- features-impl/java-base/src/com/jetbrains/ls/api/features/impl/javaBase/completion/LSCompletionItemKindProviderJavaImpl.kt
+++ features-impl/java-base/src/com/jetbrains/ls/api/features/impl/javaBase/completion/LSCompletionItemKindProviderJavaImpl.kt
@@ -1,15 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.ls.api.features.impl.javaBase.completion
 
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiEnumConstant
-import com.intellij.psi.PsiField
-import com.intellij.psi.PsiLocalVariable
-import com.intellij.psi.PsiMethod
-import com.intellij.psi.PsiPackage
-import com.intellij.psi.PsiParameter
-import com.intellij.psi.PsiTypeParameter
+import com.intellij.psi.*
 import com.jetbrains.ls.api.features.completion.LSCompletionItemKindProvider
 import com.jetbrains.lsp.protocol.CompletionItemKind
 
@@ -31,6 +23,8 @@ internal class LSCompletionItemKindProviderJavaImpl : LSCompletionItemKindProvid
             element.isEnum -> CompletionItemKind.Enum
             else -> CompletionItemKind.Class
         }
+        is PsiPrimitiveType -> CompletionItemKind.Keyword
+        is PsiKeyword -> CompletionItemKind.Keyword
 
         else -> null
     }
