8a954a122d1c9af19371cfa1d4f387a109fa35c7
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-12-16 15:45:13 +0100

    cleanup [lsp]: unify naming for implementations of MarkdownDocProvider
    GitOrigin-RevId: 2ef85ac271554630722f78562c5304e87a4b4006

--- features-impl/kotlin/src/com/jetbrains/ls/api/features/impl/common/kotlin/hover/LSKotlinMarkdownDocProvider.kt
+++ features-impl/kotlin/src/com/jetbrains/ls/api/features/impl/common/kotlin/hover/LSKotlinMarkdownDocProvider.kt
@@ -9,14 +9,15 @@ import com.jetbrains.ls.api.features.impl.common.kotlin.language.LSKotlinLanguag
 import org.jetbrains.kotlin.asJava.unwrapped
 import org.jetbrains.kotlin.psi.KtDeclaration
 
-internal class LSMarkdownDocProviderKotlinImpl : AbstractLSHoverProvider.LSMarkdownDocProvider {
+internal class LSKotlinMarkdownDocProvider : AbstractLSHoverProvider.LSMarkdownDocProvider {
     override fun getMarkdownDoc(element: PsiElement): String? {
         val ktElement = element.unwrapped as? KtDeclaration ?: return null
         val kdDocText = ktElement.docComment?.text ?: return null
+
         // TODO LSP-239 should be rendered as proper markdown
         return markdownMultilineCode(
-            formatDocComment(kdDocText),
-            language = LSKotlinLanguage.lspName
+            code = formatDocComment(kdDocText),
+            language = LSKotlinLanguage.lspName,
         )
     }
 }
