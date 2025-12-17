b223be78e543012c3bcaf5c51976928395288061
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-12-17 19:05:56 +0100

    [dap] LSP-335: Provide lsp server coroutine scope to the command handler
    GitOrigin-RevId: d6e066b306414b9c16b19a306e55f00cca969419

--- features-impl/common/src/com/jetbrains/ls/api/features/impl/common/debug/LSStartDebugCommandDescriptorProvider.kt
+++ features-impl/common/src/com/jetbrains/ls/api/features/impl/common/debug/LSStartDebugCommandDescriptorProvider.kt
@@ -4,17 +4,13 @@ package com.jetbrains.ls.api.features.impl.common.debug
 import com.jetbrains.dap.platform.startDapConnection
 import com.jetbrains.ls.api.features.commands.LSCommandDescriptor
 import com.jetbrains.ls.api.features.commands.LSCommandDescriptorProvider
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.currentCoroutineContext
+import com.jetbrains.lsp.implementation.lspScope
 import kotlinx.serialization.json.JsonPrimitive
 
 internal object LSStartDebugCommandDescriptorProvider : LSCommandDescriptorProvider {
     override val commandDescriptors: List<LSCommandDescriptor>
         get() = listOf(LSCommandDescriptor("Start debug server", "start_debug_server") {
-            // TODO: use proper coroutine scope. This one is never cancelled
-            @Suppress("RAW_SCOPE_CREATION")
-            val port = startDapConnection(CoroutineScope(currentCoroutineContext().minusKey(Job)))
+            val port = startDapConnection(lspScope)
             JsonPrimitive(port)
         })
 }
