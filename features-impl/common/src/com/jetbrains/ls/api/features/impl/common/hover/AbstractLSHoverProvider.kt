b27131e2be999a2f6b7ae00690aa99776312c56b
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-12-18 10:52:54 +0100

    cleanup [lsp]: AbstractLSHoverProvider: fix filename not matching class name
    GitOrigin-RevId: bab6f4335994396cee29dd20664bca881d446557

--- features-impl/common/src/com/jetbrains/ls/api/features/impl/common/hover/AbstractLSHoverProvider.kt
+++ features-impl/common/src/com/jetbrains/ls/api/features/impl/common/hover/AbstractLSHoverProvider.kt
@@ -1,54 +1,46 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.ls.api.features.impl.common.hover
 
-import com.intellij.codeInsight.TargetElementUtilBase
 import com.intellij.lang.Language
 import com.intellij.lang.LanguageExtension
 import com.intellij.openapi.application.readAction
 import com.intellij.openapi.editor.Document
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.findDocument
 import com.intellij.openapi.vfs.findPsiFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.jetbrains.ls.api.core.util.findVirtualFile
-import com.jetbrains.ls.api.core.util.offsetByPosition
-import com.jetbrains.ls.api.core.util.toLspRange
 import com.jetbrains.ls.api.core.LSAnalysisContext
 import com.jetbrains.ls.api.core.LSServer
 import com.jetbrains.ls.api.core.project
+import com.jetbrains.ls.api.core.util.findVirtualFile
+import com.jetbrains.ls.api.core.util.offsetByPosition
+import com.jetbrains.ls.api.core.util.toLspRange
 import com.jetbrains.ls.api.core.withAnalysisContext
 import com.jetbrains.ls.api.features.hover.LSHoverProvider
 import com.jetbrains.ls.api.features.impl.common.utils.TargetKind
 import com.jetbrains.ls.api.features.impl.common.utils.getTargetsAtPosition
 import com.jetbrains.lsp.implementation.LspHandlerContext
-import com.jetbrains.lsp.protocol.Hover
-import com.jetbrains.lsp.protocol.HoverParams
-import com.jetbrains.lsp.protocol.MarkupContent
-import com.jetbrains.lsp.protocol.MarkupKindType
-import com.jetbrains.lsp.protocol.Position
-import com.jetbrains.lsp.protocol.Range
-import com.jetbrains.lsp.protocol.StringOrMarkupContent
+import com.jetbrains.lsp.protocol.*
 
 abstract class AbstractLSHoverProvider(
     private val targetKinds: Set<TargetKind>
 ) : LSHoverProvider {
     protected open fun acceptTarget(target: PsiElement): Boolean = true
+
     context(_: LSServer, _: LspHandlerContext)
     override suspend fun getHover(params: HoverParams): Hover? {
         return withAnalysisContext {
-            readAction a@{
-                val file = params.findVirtualFile() ?: return@a null
-                val psiFile = file.findPsiFile(project) ?: return@a null
-                val document = file.findDocument() ?: return@a null
-                val targets =
-                    psiFile.getTargetsAtPosition(params.position, document, targetKinds)
-                        .filter { acceptTarget(it) }
-                if (targets.isEmpty()) return@a null
+            readAction {
+                val virtualFile = params.findVirtualFile() ?: return@readAction null
+                val psiFile = virtualFile.findPsiFile(project) ?: return@readAction null
+                val document = virtualFile.findDocument() ?: return@readAction null
+                val targets = psiFile
+                    .getTargetsAtPosition(params.position, document, targetKinds)
+                    .filter { psiElement -> acceptTarget(psiElement) }
+                if (targets.isEmpty()) return@readAction null
 
-
-                val markdown = targets.mapNotNull {
-                    generateMarkdownForPsiElementTarget(it, psiFile)
+                val markdown = targets.mapNotNull { psiElement ->
+                    generateMarkdownForPsiElementTarget(psiElement, psiFile)
                 }.joinToString("\n---\n")
 
                 Hover(
@@ -83,8 +75,7 @@ abstract class AbstractLSHoverProvider(
                 return StringOrMarkupContent(MarkupContent(MarkupKindType.Markdown, doc))
             }
 
-            fun forLanguage(language: Language): LSMarkdownDocProvider? =
-                Extension.forLanguage(language)
+            fun forLanguage(language: Language): LSMarkdownDocProvider? = Extension.forLanguage(language)
         }
     }
 }
