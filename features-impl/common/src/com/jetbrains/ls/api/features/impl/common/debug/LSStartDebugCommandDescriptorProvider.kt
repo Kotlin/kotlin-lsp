cad3f6849b39db4399c0cbd6429acc4b53af2153
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2025-12-23 16:51:08 +0100

    [dap] LSP-350: Extract TCP server to separate function
    Also, allow creating multiple dap servers
    
    GitOrigin-RevId: 766c18df5bbe7a978d84b6b1c144543f30b57e59

--- features-impl/common/src/com/jetbrains/ls/api/features/impl/common/debug/LSStartDebugCommandDescriptorProvider.kt
+++ features-impl/common/src/com/jetbrains/ls/api/features/impl/common/debug/LSStartDebugCommandDescriptorProvider.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.ls.api.features.impl.common.debug
 
-import com.jetbrains.dap.platform.startDapConnection
+import com.jetbrains.dap.platform.startDapServer
 import com.jetbrains.ls.api.features.commands.LSCommandDescriptor
 import com.jetbrains.ls.api.features.commands.LSCommandDescriptorProvider
 import com.jetbrains.lsp.implementation.lspScope
@@ -10,7 +10,7 @@ import kotlinx.serialization.json.JsonPrimitive
 internal object LSStartDebugCommandDescriptorProvider : LSCommandDescriptorProvider {
     override val commandDescriptors: List<LSCommandDescriptor>
         get() = listOf(LSCommandDescriptor("Start debug server", "start_debug_server") {
-            val port = startDapConnection(lspScope)
+            val port = startDapServer(lspScope)
             JsonPrimitive(port)
         })
 }
