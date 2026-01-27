fda75c224c02173965a6da5ef642aa7ad6bff18f
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2026-01-27 15:05:59 +0100

    [dap] LSP-469: Implement basic java completion in debug console
    GitOrigin-RevId: 9146f18fdb9982e69c8a2bda48f66f7567049664

--- features-impl/kotlin/src/com/jetbrains/ls/api/features/impl/common/kotlin/debug/JvmDapConfiguration.kt
+++ features-impl/kotlin/src/com/jetbrains/ls/api/features/impl/common/kotlin/debug/JvmDapConfiguration.kt
@@ -1,12 +1,13 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.ls.api.features.impl.common.kotlin.debug
 
 import com.jetbrains.dap.jvm.javaBackendDebuggerModule
 import com.jetbrains.dap.jvm.javaDebuggerModule
 import com.jetbrains.dap.jvm.javaSharedDebuggerModule
 import com.jetbrains.dap.jvm.jvmDapPlugin
+import com.jetbrains.dap.lsp.features.completion.jvmDapLspPlugin
 import com.jetbrains.ls.api.features.dap.DapConfigurationPiece
 
 val DapJvmConfiguration: DapConfigurationPiece = DapConfigurationPiece(
-    plugins = listOf(javaSharedDebuggerModule, javaDebuggerModule, javaBackendDebuggerModule, jvmDapPlugin),
+    plugins = listOf(javaSharedDebuggerModule, javaDebuggerModule, javaBackendDebuggerModule, jvmDapPlugin, jvmDapLspPlugin),
 )
