26bd95ab1c2eaf30761cf8b432dbc0b750815ff5
Bartek Pacia <bartek.pacia@jetbrains.com>
2026-01-30 21:23:30 +0100

    LSP-450 extract `sendLogToClient` method for better separation of logging responsibilities
    GitOrigin-RevId: 448f1688cd912798f63cc2bb8815f68b64aac4d4

--- kotlin-lsp/src/com/jetbrains/ls/kotlinLsp/logging/logging.kt
+++ kotlin-lsp/src/com/jetbrains/ls/kotlinLsp/logging/logging.kt
@@ -87,7 +87,7 @@ private class LspLogger(
 
     private fun log(level: LogLevel, message: String?, t: Throwable?, details: Array<out String?> = emptyArray()) {
         if (!shouldLog(level)) return
-        val messageRendered by lazy(LazyThreadSafetyMode.NONE) {
+        val renderedMessage by lazy(LazyThreadSafetyMode.NONE) {
             buildString {
                 val currentMillis = System.currentTimeMillis()
                 if (withDateTime) {
@@ -133,46 +133,57 @@ private class LspLogger(
             }
         }
 
+        // Send log to stdout
         if (writeToStdOut && shouldLog(level)) {
-            println(messageRendered)
+            println(renderedMessage)
         }
 
+        // Send log to the connected client, using
         Client.current?.let { client ->
-            fun logMessage(messageType: MessageType) {
-                client.lspClient.notifyAsync(
-                    notificationType = LogMessageNotification,
-                    params = LogMessageParams(messageType, messageRendered),
-                )
-            }
-            when (level) {
-                LogLevel.OFF -> {}
-                LogLevel.ERROR -> logMessage(MessageType.Error)
-                LogLevel.WARNING -> logMessage(MessageType.Warning)
-                LogLevel.INFO -> logMessage(MessageType.Info)
-                LogLevel.DEBUG, LogLevel.TRACE -> {
-                    // send debug trace to the client
-                    val shouldNotifyForDebug = when (client.trace) {
-                        TraceValue.Off, null -> false
-                        TraceValue.Messages, TraceValue.Verbose -> true
-                    }
-                    if (shouldNotifyForDebug) {
-                        client.lspClient.notifyAsync(
-                            notificationType = LogTraceNotificationType,
-                            params = LogTraceParams(
-                                message = messageRendered,
-                                verbose = null, // TODO: LSP-229 provide more details here?
-                            ),
-                        )
-                    }
-                }
-
-                LogLevel.ALL -> {}
-            }
+            sendLogToClient(client, renderedMessage)
         }
+
         if (t != null && shouldRethrow(t)) {
             throw t
         }
     }
+
+    /**
+     * Sends an LSP notification containing [renderedMessage] to the connected [client].
+     */
+    private fun sendLogToClient(client: Client, renderedMessage: String) {
+        fun logMessage(messageType: MessageType) {
+            client.lspClient.notifyAsync(
+                notificationType = LogMessageNotification,
+                params = LogMessageParams(messageType, renderedMessage),
+            )
+        }
+        when (level) {
+            LogLevel.OFF -> {}
+            LogLevel.ERROR -> logMessage(MessageType.Error)
+            LogLevel.WARNING -> logMessage(MessageType.Warning)
+            LogLevel.INFO -> logMessage(MessageType.Info)
+            LogLevel.DEBUG, LogLevel.TRACE -> {
+                // send debug trace to the client
+                val shouldNotifyForDebug = when (client.trace) {
+                    TraceValue.Off, null -> false
+                    TraceValue.Messages, TraceValue.Verbose -> true
+                }
+                if (shouldNotifyForDebug) {
+                    client.lspClient.notifyAsync(
+                        notificationType = LogTraceNotificationType,
+                        params = LogTraceParams(
+                            message = renderedMessage,
+                            verbose = null, // TODO: LSP-229 provide more details here?
+                        ),
+                    )
+                }
+            }
+
+            LogLevel.ALL -> {}
+        }
+    }
+
 
     private fun currentTraceValue(): TraceValue = Client.current?.trace ?: TraceValue.Off
 
