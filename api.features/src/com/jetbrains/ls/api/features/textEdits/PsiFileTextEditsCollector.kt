03b391e6d3d107384f783898e1edcebb30ccde5a
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2026-02-12 21:49:34 +0100

    refactor [Analyzer]: Replace PsiFileTextEditsCollector with coroutine based approach
    GitOrigin-RevId: d3ffd1d81223fc081873a22417b7bf236bfc4d6c

--- api.features/src/com/jetbrains/ls/api/features/textEdits/PsiFileTextEditsCollector.kt
+++ api.features/src/com/jetbrains/ls/api/features/textEdits/PsiFileTextEditsCollector.kt
@@ -4,8 +4,8 @@ package com.jetbrains.ls.api.features.textEdits
 import com.intellij.lang.Language
 import com.intellij.lang.LanguageExtension
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.invokeAndWaitIfNeeded
-import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.writeAction
 import com.intellij.openapi.command.CommandProcessor
 import com.intellij.openapi.diagnostic.getOrHandleException
 import com.intellij.openapi.diagnostic.logger
@@ -16,18 +16,20 @@ import com.jetbrains.ls.api.core.LSAnalysisContext
 import com.jetbrains.ls.api.core.project
 import com.jetbrains.ls.api.core.util.uri
 import com.jetbrains.lsp.protocol.TextEdit
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 object PsiFileTextEditsCollector {
 
     private val logger = logger<PsiFileTextEditsCollector>()
 
     context(analysisContext: LSAnalysisContext)
-    fun collectTextEdits(file: VirtualFile, modificationAction: (file: PsiFile) -> Unit): List<TextEdit> {
+    suspend fun collectTextEdits(file: VirtualFile, modificationAction: (file: PsiFile) -> Unit): List<TextEdit> {
         val app = ApplicationManager.getApplication()
         app.assertReadAccessNotAllowed()
 
-        return invokeAndWaitIfNeeded {
-            runWriteAction {
+        return withContext(Dispatchers.EDT) {
+            writeAction {
                 var res: List<TextEdit>? = null
                 CommandProcessor.getInstance().executeCommand(
                     project,
