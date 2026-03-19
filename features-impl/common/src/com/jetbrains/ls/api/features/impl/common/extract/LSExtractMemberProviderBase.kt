0d9db24ac1b94c97700d6a4305d5e1a234d2af7e
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2026-03-19 17:07:50 +0100

    [Java] LSP-819 Navigation to extracted variable/method
    GitOrigin-RevId: d5185819825ef6675ccee315c841ae48c9b5e5cb

--- features-impl/common/src/com/jetbrains/ls/api/features/impl/common/extract/LSExtractMemberProviderBase.kt
+++ features-impl/common/src/com/jetbrains/ls/api/features/impl/common/extract/LSExtractMemberProviderBase.kt
@@ -6,7 +6,9 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.findDocument
+import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.PostprocessReformattingAspect
+import com.intellij.psi.util.startOffset
 import com.intellij.util.concurrency.annotations.RequiresReadLock
 import com.jetbrains.ls.api.core.LSAnalysisContext
 import com.jetbrains.ls.api.core.LSServer
@@ -19,10 +21,12 @@ import com.jetbrains.ls.api.core.withWriteAnalysisContextAndFileSettings
 import com.jetbrains.ls.api.features.codeActions.LSCodeActionProvider
 import com.jetbrains.ls.api.features.commands.LSCommandDescriptor
 import com.jetbrains.ls.api.features.commands.LSCommandDescriptorProvider
-import com.jetbrains.ls.api.features.commands.document.LSDocumentCommandExecutor
+import com.jetbrains.ls.api.features.commands.LSCommandExecutor
 import com.jetbrains.ls.api.features.textEdits.TextEditsComputer.computeTextEdits
 import com.jetbrains.lsp.implementation.LspHandlerContext
 import com.jetbrains.lsp.implementation.lspClient
+import com.jetbrains.lsp.protocol.ApplyEditRequests
+import com.jetbrains.lsp.protocol.ApplyWorkspaceEditParams
 import com.jetbrains.lsp.protocol.CodeAction
 import com.jetbrains.lsp.protocol.CodeActionKind
 import com.jetbrains.lsp.protocol.CodeActionParams
@@ -31,13 +35,18 @@ import com.jetbrains.lsp.protocol.DocumentUri
 import com.jetbrains.lsp.protocol.LSP
 import com.jetbrains.lsp.protocol.MessageType
 import com.jetbrains.lsp.protocol.Range
+import com.jetbrains.lsp.protocol.ShowDocument
+import com.jetbrains.lsp.protocol.ShowDocumentParams
 import com.jetbrains.lsp.protocol.ShowMessageNotificationType
 import com.jetbrains.lsp.protocol.ShowMessageParams
 import com.jetbrains.lsp.protocol.TextEdit
+import com.jetbrains.lsp.protocol.WorkspaceEdit
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.encodeToJsonElement
 
 /**
@@ -57,15 +66,12 @@ abstract class LSExtractMemberProviderBase<Context> : LSCodeActionProvider, LSCo
             LSCommandDescriptor(
                 title = descriptorTitle,
                 name = commandName,
-                executor = object : LSDocumentCommandExecutor {
+                executor = object : LSCommandExecutor {
                     context(server: LSServer, handlerContext: LspHandlerContext)
-                    override suspend fun executeForDocument(
-                        documentUri: DocumentUri,
-                        otherArgs: List<JsonElement>
-                    ): List<TextEdit> {
-                        val payload = otherArgs.firstOrNull()?.let {
-                            LSP.json.decodeFromJsonElement(Payload.serializer(), it)
-                        } ?: return emptyList()
+                    override suspend fun execute(arguments: List<JsonElement>): JsonElement {
+                        require(arguments.size == 2) { "Expected 2 arguments, got: ${arguments.size}" }
+                        val documentUri = LSP.json.decodeFromJsonElement<DocumentUri>(arguments.first())
+                        val payload = LSP.json.decodeFromJsonElement<Payload>(arguments.last())
 
                         when (payload) {
                             is Payload.Error -> {
@@ -76,18 +82,44 @@ abstract class LSExtractMemberProviderBase<Context> : LSCodeActionProvider, LSCo
                                         payload.message,
                                     )
                                 )
-                                return emptyList()
                             }
+
                             is Payload.Data -> {
-                                return server.withWriteAnalysisContextAndFileSettings(documentUri.uri) {
+                                val result = server.withWriteAnalysisContextAndFileSettings(documentUri.uri) {
                                     val (file, data) = readAction {
                                         val virtualFile = documentUri.findVirtualFile() ?: return@readAction null
                                         virtualFile to payload
-                                    } ?: return@withWriteAnalysisContextAndFileSettings emptyList()
-                                    computeExtractEdits(file, data)
+                                    } ?: return@withWriteAnalysisContextAndFileSettings null
+                                    computeExtractResult(file, data)
+                                }
+
+                                if (result == null || result.edits.isEmpty()) return JsonPrimitive(true)
+
+                                lspClient.request(
+                                    ApplyEditRequests.ApplyEdit,
+                                    ApplyWorkspaceEditParams(
+                                        label = null,
+                                        edit = WorkspaceEdit(
+                                            changes = mapOf(documentUri to result.edits)
+                                        ),
+                                    ),
+                                )
+
+                                if (result.navigationRange != null) {
+                                    lspClient.request(
+                                        requestType = ShowDocument,
+                                        params = ShowDocumentParams(
+                                            uri = documentUri.uri,
+                                            external = false,
+                                            takeFocus = true,
+                                            selection = result.navigationRange,
+                                        ),
+                                    )
                                 }
                             }
                         }
+
+                        return JsonPrimitive(true)
                     }
                 },
             ),
@@ -124,15 +156,16 @@ abstract class LSExtractMemberProviderBase<Context> : LSCodeActionProvider, LSCo
                             command = Command(
                                 title = choice,
                                 command = commandName,
-                                arguments = listOf<JsonElement>(
+                                arguments = listOf(
                                     LSP.json.encodeToJsonElement<DocumentUri>(documentUri),
-                                    LSP.json.encodeToJsonElement(Payload.serializer(), Payload.Data(selectionRange, choice)),
+                                    LSP.json.encodeToJsonElement<Payload>(Payload.Data(selectionRange, choice)),
                                 ),
                             )
                         )
                     )
                 }
             }
+
             is ChoicesResult.Error -> {
                 emit(
                     CodeAction(
@@ -141,9 +174,9 @@ abstract class LSExtractMemberProviderBase<Context> : LSCodeActionProvider, LSCo
                         command = Command(
                             title = choicesResult.defaultTitle,
                             command = commandName,
-                            arguments = listOf<JsonElement>(
+                            arguments = listOf(
                                 LSP.json.encodeToJsonElement<DocumentUri>(documentUri),
-                                LSP.json.encodeToJsonElement(Payload.serializer(), Payload.Error(choicesResult.errorMessage)),
+                                LSP.json.encodeToJsonElement<Payload>(Payload.Error(choicesResult.errorMessage)),
                             ),
                         )
                     )
@@ -153,24 +186,28 @@ abstract class LSExtractMemberProviderBase<Context> : LSCodeActionProvider, LSCo
     }
 
     context(server: LSServer, analysisContext: LSAnalysisContext)
-    private suspend fun computeExtractEdits(file: VirtualFile, data: Payload.Data): List<TextEdit> {
+    private suspend fun computeExtractResult(file: VirtualFile, data: Payload.Data): ExtractResult {
         val (writeContext, oldDocumentText) = readAction {
             val document = file.findDocument() ?: return@readAction null
             val selection = data.selection.toTextRange(document)
             getWriteContext(file, selection, data.choice) to document.text
-        } ?: return emptyList()
+        } ?: return ExtractResult(emptyList(), null)
 
-        server.withWritableFile(file.uri) {
+        val navigationRange = server.withWritableFile(file.uri) {
             val postProcessReformatting = PostprocessReformattingAspect.getInstance(project)
             if (postProcessReformatting == null) {
                 LOG.error("Wasn't able to initialize PostProcessReformattingAspect")
             }
-            doExtract(writeContext)
+            val reference = doExtract(writeContext) ?: return@withWritableFile null
+            TextRange(reference.startOffset, reference.startOffset)
         }
 
         return readAction {
-            val document = file.findDocument() ?: return@readAction emptyList()
-            computeTextEdits(oldDocumentText, document.text)
+            val document = file.findDocument() ?: return@readAction ExtractResult.EMPTY
+            ExtractResult(
+                computeTextEdits(oldDocumentText, document.text),
+                navigationRange?.toLspRange(document)
+            )
         }
     }
 
@@ -197,9 +234,17 @@ abstract class LSExtractMemberProviderBase<Context> : LSCodeActionProvider, LSCo
     /**
      * Executes the extraction on the given [context].
      * After this method is called, the member is extracted and computation of the edits is performed.
+     *
+     * @return the element to which the caret position should be navigated
      */
     context(server: LSServer, analysisContext: LSAnalysisContext)
-    protected abstract suspend fun doExtract(context: Context)
+    protected abstract suspend fun doExtract(context: Context): PsiElement?
+
+    private data class ExtractResult(val edits: List<TextEdit>, val navigationRange: Range?) {
+        companion object {
+            val EMPTY = ExtractResult(emptyList(), null)
+        }
+    }
 
     protected sealed interface ChoicesResult {
         data class Choices(val choices: List<String>, val selection: TextRange) : ChoicesResult
@@ -215,7 +260,7 @@ abstract class LSExtractMemberProviderBase<Context> : LSCodeActionProvider, LSCo
         data class Error(val message: String) : Payload
     }
 
-    companion object{
+    companion object {
         private val LOG = Logger.getInstance(LSExtractMemberProviderBase::class.java)
     }
 }
