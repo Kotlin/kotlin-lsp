17cb7d34365192eaaa3ff12e0cf2f13e4b14af70
pavel.mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2026-04-14 21:07:38 +0200

    LSP-832: Smart handling of Enter keypresses
    GitOrigin-RevId: 4af5049d55b086281930f82221d7d24676b41c5b

--- kotlin-vscode/src/types.ts
+++ kotlin-vscode/src/types.ts
@@ -1,12 +1,35 @@
 import { Tree } from 'web-tree-sitter';
 
-export type KeyHandler = (tree: Tree, key: string, offset: number) => KeyResult;
-
-export interface KeyResult {
-    text: string
-    offset: number
+export interface KeyEdit {
+    startOffset: number
+    endOffset: number
+    newText: string
 }
 
-export function keyResult(text: string, offset: number): KeyResult {
-    return {text, offset};
+export interface KeyResult {
+    edits: KeyEdit[]
+    caretOffset: number
+}
+
+export type KeyHandler = (text: string, tree: Tree, key: string, offset: number, indentUnit: string) => KeyResult;
+
+export function keyResult(text: string, offset: number, index: number, deleteLeft = 0, deleteRight = 0): KeyResult {
+    const startOffset = index - deleteLeft;
+    const endOffset = index + 1 + deleteRight;
+    return {
+        edits: [{
+            startOffset,
+            endOffset,
+            newText: text,
+        }],
+        caretOffset: startOffset + offset,
+    };
+}
+
+export function isIdentityKeyResult(index: number, key: string, result: KeyResult): boolean {
+    return result.edits.length === 1 &&
+            result.edits[0].startOffset === index &&
+            result.edits[0].endOffset === index + key.length &&
+            result.edits[0].newText === key &&
+            result.caretOffset === index + key.length;
 }
