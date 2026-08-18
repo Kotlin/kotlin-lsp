182085de773b66b99f680bb143b4ff3a82cadc3e
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2026-08-18 17:03:58 +0200

    [Kotlin] LSP-1319 Move single kotlin file
    - Finish implementation
    
    GitOrigin-RevId: 1731693c709add8ba784f20b70077d99e80b48b3

--- features-impl/kotlin/src/com/jetbrains/ls/api/features/impl/kotlin/move/LSMoveKotlinFileProvider.kt
+++ features-impl/kotlin/src/com/jetbrains/ls/api/features/impl/kotlin/move/LSMoveKotlinFileProvider.kt
@@ -2,12 +2,17 @@
 package com.jetbrains.ls.api.features.impl.kotlin.move
 
 import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.jetbrains.ls.api.core.LSAnalysisContext
 import com.jetbrains.ls.api.features.impl.common.move.LSMoveFileProviderBase
 import com.jetbrains.ls.api.features.impl.common.processors.RefactoringProcessor
 import com.jetbrains.ls.api.features.impl.kotlin.language.LSKotlinLanguage
 import com.jetbrains.ls.api.features.impl.kotlin.processors.MoveKotlinFileProcessor
+import org.jetbrains.kotlin.idea.base.util.KotlinSingleClassFileAnalyzer
+import org.jetbrains.kotlin.idea.k2.refactoring.move.processor.canMove
+import org.jetbrains.kotlin.idea.k2.refactoring.move.ui.K2MoveModel
+import org.jetbrains.kotlin.psi.KtFile
 
 internal object LSMoveKotlinFileProvider : LSMoveFileProviderBase(setOf(LSKotlinLanguage)) {
     context(_: LSAnalysisContext)
@@ -15,6 +20,20 @@ internal object LSMoveKotlinFileProvider : LSMoveFileProviderBase(setOf(LSKotlin
         targetDirectory: PsiDirectory,
         file: PsiFile
     ): RefactoringProcessor? {
-        return MoveKotlinFileProcessor.create()
+        if (file !is KtFile) return null
+        val clazz = KotlinSingleClassFileAnalyzer.getSingleClass(file)
+
+        val targets: Array<PsiElement> = if (clazz != null && clazz.containingKtFile.declarations.size == 1) arrayOf(clazz) else arrayOf(file)
+        if (!canMove(targets)) return null
+
+        val model = K2MoveModel.create(
+            targets,
+            targetDirectory,
+            null,
+            null,
+            false
+        ) ?: return null
+
+        return MoveKotlinFileProcessor.create(model)
     }
 }
--- features-impl/kotlin/src/com/jetbrains/ls/api/features/impl/kotlin/processors/MoveKotlinFileProcessor.kt
+++ features-impl/kotlin/src/com/jetbrains/ls/api/features/impl/kotlin/processors/MoveKotlinFileProcessor.kt
@@ -1,55 +1,117 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.ls.api.features.impl.kotlin.processors
 
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Ref
+import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.refactoring.listeners.RefactoringElementListener
 import com.intellij.refactoring.listeners.RefactoringEventData
 import com.intellij.refactoring.listeners.impl.RefactoringTransaction
 import com.intellij.refactoring.move.moveFilesOrDirectories.MoveFileHandler
+import com.intellij.refactoring.move.moveFilesOrDirectories.MoveFilesOrDirectoriesUtil
+import com.intellij.refactoring.util.NonCodeUsageInfo
 import com.intellij.usageView.UsageInfo
 import com.intellij.util.containers.MultiMap
 import com.jetbrains.ls.api.features.impl.common.processors.RefactoringProcessor
+import org.jetbrains.kotlin.analysis.api.permissions.KaAllowAnalysisFromWriteAction
+import org.jetbrains.kotlin.analysis.api.permissions.KaAllowAnalysisOnEdt
+import org.jetbrains.kotlin.analysis.api.permissions.allowAnalysisFromWriteAction
+import org.jetbrains.kotlin.analysis.api.permissions.allowAnalysisOnEdt
+import org.jetbrains.kotlin.idea.k2.refactoring.move.ui.K2MoveModel
 
 /**
  * @see com.intellij.refactoring.move.moveFilesOrDirectories.MoveFilesOrDirectoriesProcessor
+ * @see org.jetbrains.kotlin.idea.k2.refactoring.move.processor.K2MoveFilesOrDirectoriesRefactoringProcessor
  */
-internal class MoveKotlinFileProcessor : RefactoringProcessor {
+internal class MoveKotlinFileProcessor(
+    private val project: Project,
+    private val elementsToMove: Array<PsiElement>,
+    private val targetDirectory: PsiDirectory,
+    private val searchForReferences: Boolean,
+    private val searchForComments: Boolean,
+    private val searchForTextOccurrences: Boolean
+) : RefactoringProcessor {
+    private val classifiedUsages : MutableMap<PsiFile, List<UsageInfo>> = mutableMapOf()
+
     override fun collectConflicts(
         refUsages: Ref<Array<UsageInfo>>,
         conflicts: MultiMap<PsiElement, String>
     ) {
-        //
-        MoveFileHandler.detectConflicts(emptyArray(), refUsages.get(), null, conflicts)
+        MoveFileHandler.detectConflicts(elementsToMove, refUsages.get(), targetDirectory, conflicts)
     }
 
     override fun findUsages(): Array<UsageInfo>? {
-        TODO("not implemented")
+        val usagesContext = MoveFilesOrDirectoriesUtil.findUsages(
+            project,
+            elementsToMove,
+            targetDirectory,
+            searchForReferences,
+            searchForComments,
+            searchForTextOccurrences
+        )
+        classifiedUsages.putAll(usagesContext.classifiedUsages)
+        return usagesContext.allUsages.toTypedArray()
     }
 
-    override fun processUsages(initialUsages: Array<UsageInfo>): Array<UsageInfo> {
-        TODO("not implemented")
-    }
+    override fun processUsages(initialUsages: Array<UsageInfo>): Array<UsageInfo> = initialUsages
 
     override fun getFilesToSave(usages: Array<UsageInfo>): List<PsiFile> {
-        TODO("not implemented")
+        return usages.mapNotNull { it.file } + elementsToMove.mapNotNull { it.containingFile }
     }
 
+    @OptIn(KaAllowAnalysisOnEdt::class, KaAllowAnalysisFromWriteAction::class)
     override fun performRefactoring(
         usages: Array<UsageInfo>,
         transaction: RefactoringTransaction
     ) {
-        TODO("not implemented")
+        allowAnalysisOnEdt {
+            allowAnalysisFromWriteAction {
+                doPerformRefactoring(usages, transaction)
+            }
+        }
+    }
+
+    private fun doPerformRefactoring(
+        usages: Array<UsageInfo>,
+        transaction: RefactoringTransaction
+    ) {
+        val codeUsages = usages.filter { it !is NonCodeUsageInfo }
+
+        val listeners: List<RefactoringElementListener> =
+            elementsToMove.map { item: PsiElement -> transaction.getElementListener(item) }
+
+        val result = MoveFilesOrDirectoriesUtil.moveElements(
+            project, elementsToMove,
+            targetDirectory,
+            ProgressManager.getInstance().getProgressIndicator(),
+            searchForReferences
+        )
+
+        MoveFilesOrDirectoriesUtil.retargetCodeUsages(codeUsages.toTypedArray())
+        MoveFilesOrDirectoriesUtil.retargetClassifiedUsages(classifiedUsages, result.oldToNewMap)
+
+        MoveFilesOrDirectoriesUtil.afterMovement(listeners, result.movedElementPointers)
     }
 
 
     override fun createEventData(): RefactoringEventData {
-        TODO("not implemented")
+        return RefactoringEventData().apply { addElements(elementsToMove) }
     }
 
     companion object {
-        fun create(): RefactoringProcessor? {
-            return null
+        fun create(model : K2MoveModel): RefactoringProcessor {
+            val descriptor = model.toDescriptor()
+            return MoveKotlinFileProcessor(
+                project = descriptor.project,
+                elementsToMove = descriptor.sourceElements.toTypedArray(),
+                targetDirectory = descriptor.moveDescriptors.first().target.getOrCreateTarget(descriptor.dirStructureMatchesPkg) as PsiDirectory,
+                searchForReferences = descriptor.searchReferences,
+                searchForComments = descriptor.searchInComments,
+                searchForTextOccurrences = descriptor.searchForText
+            )
         }
     }
 }
