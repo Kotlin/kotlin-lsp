b445bbeeb7286722bdf09fef906dbc353ca1c5a6
Georgii Ustinov <georgii.ustinov@jetbrains.com>
2026-09-14 17:27:13 +0200

    [Java, Kotlin] LSP-1852 Provide user visible error message at early stage of move refactoring
    GitOrigin-RevId: bc71f64c82726e792c86bc63c01d6daca86e52b6

--- features-impl/common/src/com/jetbrains/ls/api/features/impl/common/move/LSCommonMoveProvider.kt
+++ features-impl/common/src/com/jetbrains/ls/api/features/impl/common/move/LSCommonMoveProvider.kt
@@ -6,45 +6,66 @@ import com.intellij.openapi.vfs.findPsiDirectory
 import com.intellij.openapi.vfs.findPsiFile
 import com.jetbrains.ls.api.core.LSServer
 import com.jetbrains.ls.api.core.project
+import com.jetbrains.ls.api.core.util.fileName
 import com.jetbrains.ls.api.core.util.findVirtualFile
+import com.jetbrains.ls.api.features.LspServerBundle
+import com.jetbrains.ls.api.features.impl.common.processors.LSRefactoringProcessor
 import com.jetbrains.ls.api.features.impl.common.processors.doRefactoring
 import com.jetbrains.ls.api.features.impl.common.utils.findDestination
 import com.jetbrains.ls.api.features.move.LSMoveProvider
 import com.jetbrains.ls.api.features.textEdits.TextEditsComputer
 import com.jetbrains.lsp.implementation.LspHandlerContext
+import com.jetbrains.lsp.implementation.lspClient
+import com.jetbrains.lsp.implementation.throwLspError
+import com.jetbrains.lsp.protocol.ErrorCodes
 import com.jetbrains.lsp.protocol.FileRename
+import com.jetbrains.lsp.protocol.MessageType
+import com.jetbrains.lsp.protocol.RenameRequestType
+import com.jetbrains.lsp.protocol.ShowMessageNotificationType
+import com.jetbrains.lsp.protocol.ShowMessageParams
 import com.jetbrains.lsp.protocol.WorkspaceEdit
+import org.jetbrains.annotations.Nls
 
 /**
  * Follows the logic of [com.intellij.refactoring.move.MoveHandler] but with the adaptation to the LSP.
  */
 internal object LSCommonMoveProvider : LSMoveProvider {
     context(server: LSServer, handlerContext: LspHandlerContext)
-    override suspend fun moveFile(params: List<FileRename>): WorkspaceEdit? = doMove(params, false)
+    override suspend fun moveFile(params: List<FileRename>): WorkspaceEdit = doMove(params, false)
 
     context(server: LSServer, handlerContext: LspHandlerContext)
-    override suspend fun moveDirectory(params: List<FileRename>): WorkspaceEdit? = doMove(params, true)
+    override suspend fun moveDirectory(params: List<FileRename>): WorkspaceEdit = doMove(params, true)
 
     context(server: LSServer, handlerContext: LspHandlerContext)
     private suspend fun doMove(
         params: List<FileRename>,
         isOnlyDirectories : Boolean
-    ): WorkspaceEdit? {
+    ): WorkspaceEdit {
         val changes = server.withWriteAnalysisContext {
-            val processor = readAction {
-                val targetDirectory = findDestination(project, params) ?: return@readAction null
+            val processorOrError: ProcessorOrError = readAction {
+                val existedFile = params.find { it.newUri.findVirtualFile() != null }
+                if (existedFile != null) return@readAction ProcessorOrError.Error(LspServerBundle.message("error.move.file.exists.in.destination", existedFile.newUri.fileName))
+
+                val targetDirectory = findDestination(project, params) ?: return@readAction ProcessorOrError.Error(LspServerBundle.message("error.move.destination.not.found"))
 
                 val sources = params.map {
-                    val vFile = it.oldUri.findVirtualFile() ?: return@readAction null
+                    val vFile = it.oldUri.findVirtualFile() ?: return@readAction ProcessorOrError.Error(LspServerBundle.message("error.move.file.not.found", it.oldUri.fileName))
 
                     when {
                         vFile.isDirectory -> vFile.findPsiDirectory(project)
                         !isOnlyDirectories -> vFile.findPsiFile(project)
                         else -> null
-                    } ?: return@readAction null
+                    } ?: return@readAction ProcessorOrError.Error(LspServerBundle.message("error.move.file.not.found", it.oldUri.fileName))
                 }
 
-                if (sources.distinctBy { it.name }.size != sources.size) return@readAction null
+                val classified = sources.groupBy { it.name }
+                if (classified.size != sources.size) {
+                    val duplicate = classified.firstNotNullOf { (_, value) ->
+                        if (value.size > 1) value.first().name else null
+                    }
+
+                    return@readAction ProcessorOrError.Error(LspServerBundle.message("error.move.files.with.same.name", duplicate))
+                }
 
                 val extensions = if (isOnlyDirectories) LSMoveHandlerDelegate.forDirectories() else LSMoveHandlerDelegate.forFiles()
                 val candidates = sources.map { element ->
@@ -54,13 +75,36 @@ internal object LSCommonMoveProvider : LSMoveProvider {
 
                 val handler = extensions.find { it.canMove(candidates, targetDirectory) } ?: LSGenericMoveHandlerDelegate
 
-                handler.createProcessor(candidates, targetDirectory)
-            } ?: return@withWriteAnalysisContext null
+                val processor = handler.createProcessor(candidates, targetDirectory)
+                if (processor != null) ProcessorOrError.Processor(processor = processor) else error("unable to create processor")
+            }
 
-
-            doRefactoring(processor = processor, granularity = TextEditsComputer.DiffGranularity.WORD, uriToSkip = params.map { it.oldUri }, true)
-        } ?: return null
+            when (processorOrError) {
+                is ProcessorOrError.Error -> failMove(processorOrError.message)
+                is ProcessorOrError.Processor -> doRefactoring(processor = processorOrError.processor, granularity = TextEditsComputer.DiffGranularity.WORD, uriToSkip = params.map { it.oldUri }, true)
+            }
+        }
 
         return WorkspaceEdit(documentChanges = changes)
+    }
+
+    context(_: LspHandlerContext)
+    private suspend fun failMove(message: @Nls String): Nothing {
+        lspClient.notify(
+            ShowMessageNotificationType,
+            ShowMessageParams(
+                MessageType.Error,
+                message = message,
+            )
+        )
+        throwLspError(RenameRequestType, message, Unit, ErrorCodes.InvalidParams)
+    }
+
+    private sealed interface ProcessorOrError {
+        @JvmInline
+        value class Processor(val processor: LSRefactoringProcessor) : ProcessorOrError
+
+        @JvmInline
+        value class Error(val message: String) : ProcessorOrError
     }
 }
--- features-impl/common/src/com/jetbrains/ls/api/features/impl/common/utils/move.kt
+++ features-impl/common/src/com/jetbrains/ls/api/features/impl/common/utils/move.kt
@@ -6,7 +6,6 @@ import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.findPsiDirectory
 import com.intellij.psi.PsiDirectory
 import com.intellij.util.concurrency.annotations.RequiresReadLock
-import com.jetbrains.ls.api.core.util.findVirtualFile
 import com.jetbrains.ls.api.core.util.toPath
 import com.jetbrains.lsp.protocol.FileRename
 
@@ -17,8 +16,6 @@ import com.jetbrains.lsp.protocol.FileRename
  */
 @RequiresReadLock
 fun findDestination(project: Project, params: List<FileRename>): PsiDirectory? {
-    if (params.any { it.newUri.findVirtualFile() != null }) return null
-
     val files = params.map {
         val parent = it.newUri.toPath()?.parent ?: return null
         VirtualFileManager.getInstance().findFileByNioPath(parent) ?: return null
